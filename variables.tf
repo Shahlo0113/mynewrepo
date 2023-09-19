287dd574f1ff3203a587f514190f7d595bda3a55
Shahlo0113 <shahlosuvonqulova@gmail.com>
2023-09-19 16:21:09 -0400

    homework files

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "image_id" {
+  description = "This variable contains image id"
+  type        = string
+  default     = "ami-09d95fab7fff3776c"
+}
+
+variable "instance_type" {
+    type = string
+    description = "This is my instanc type"
+}
+
+
+variable "key_name" {
+    type = string
+    description = "Name of ssh key"
+}
